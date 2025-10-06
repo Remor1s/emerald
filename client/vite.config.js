f55c949a203e9b13337acd784327bfc0dd1cc4b7
Remor1s <168758957+Remor1s@users.noreply.github.com>
2025-10-06 07:18:31 +0300

    Fix base path for GitHub Pages deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // base: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : '/golden/',
-  base: '/',
+  base: '/emerald/',
   publicDir: '../images',
   plugins: [react()],
   server: { port: 8080, host: true },
